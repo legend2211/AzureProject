7477df5bab96776281ce342c4bcd34be41ef27a6
Dnyanesh Agarkar <121270109+legend2211@users.noreply.github.com>
2024-03-20 17:33:05 +0530

    Update register1.php

--- register1.php
+++ register1.php
@@ -1,61 +1,46 @@
 <?php
-  
-  $fname=$_POST['fname'];
-  $lname=$_POST['lname'];
-  $cno=$_POST['cno'];
-  $email=$_POST['email'];
-  $bgroup=$_POST['bgroup'];
-  $date=$_POST['date'];
-  $ans=$_POST['ans'];
-  $que=$_POST['que'];
-  $pass=$_POST['pass'];
 
-  try
-{
-    /*$host="localhost";
-    $port=3306;
-    $dbname="project";
-    $dbuser="root";
-    $dbpass="root";
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$cno = $_POST['cno'];
+$email = $_POST['email'];
+$bgroup = $_POST['bgroup'];
+$date = $_POST['date'];
+$ans = $_POST['ans'];
+$que = $_POST['que'];
+$pass = $_POST['pass'];
 
-    $cn=new PDO("mysql:host=$host; port=$port; dbname=$dbname",$dbuser,$dbpass);*/
-   $host="myazsqldemo.mysql.database.azure.com";
-    $port=3306;
-    $dbname="project";
-    $dbuser="myadmin@myazsqldemo";
-    $dbpass="Server@1";
+try {
+    $host = "myazsqldemo.mysql.database.azure.com";
+    $port = 3306;
+    $dbname = "project";
+    $dbuser = "myadmin@myazsqldemo";
+    $dbpass = "Server@1";
 
     $cn = mysqli_init();
-    mysqli_ssl_set($cn,NULL,NULL,"DigiCertGlobalRootG2.crt.pem",NULL,NULL );
-}
-
-catch(PDOException $e)
-{
-    echo $e-> getmessage();
+    mysqli_ssl_set($cn, NULL, NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+    mysqli_real_connect($cn, $host, $dbuser, $dbpass, $dbname, $port);
+} catch (Exception $e) {
+    echo $e->getMessage();
     die();
 }
 
-$sql="insert into register(firstname,lastname,contactno,email,bloodgroup,passward,recoveryque,answer,lasttime) values('$fname','$lname',$cno,'$email','$bgroup',$pass,'$que','$ans','$date')";
-$result=$cn->query($sql);
+$sql = "INSERT INTO register (firstname, lastname, contactno, email, bloodgroup, passward, recoveryque, answer, lasttime) 
+        VALUES ('$fname', '$lname', '$cno', '$email', '$bgroup', '$pass', '$que', '$ans', '$date')";
+$result = mysqli_query($cn, $sql);
 
-$rowcount=$result->rowCount();
-
-if($rowcount==1)
-{
-   ?>
+if ($result) {
+    ?>
     <script type="text/javascript">alert('Successfully Registered !!!');</script>
     <?php
-    header("location:home.php");
-}
-else 
-{
+    header("location: home.php");
+} else {
     ?>
-    <script type="text/javascript">alert('please register First !!!');</script>
+    <script type="text/javascript">alert('Please register first !!!');</script>
     <?php
-    header("location:register1.html");
+    header("location: register1.html");
 }
 
-
-
+mysqli_close($cn);
 
 ?>
