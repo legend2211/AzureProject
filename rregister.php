3bd87a5ad1dba842363670c1e3a375d7ec6bf0cb
Dnyanesh Agarkar <121270109+legend2211@users.noreply.github.com>
2024-03-20 20:08:13 +0530

    Update rregister.php

--- rregister.php
+++ rregister.php
@@ -1,10 +1,11 @@
 <?php 
-  $fname=$_POST['fname'];
-  $lname=$_POST['lname'];
-  $cno=$_POST['cno'];
-  $email=$_POST['email'];
-  $bgroup=$_POST['bgroup'];
- try {
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$cno = $_POST['cno'];
+$email = $_POST['email'];
+$bgroup = $_POST['bgroup'];
+
+try {
     $host = "myazsqldemo.mysql.database.azure.com";
     $port = 3306;
     $dbname = "project";
@@ -13,26 +14,26 @@
 
     $cn = mysqli_init();
     mysqli_ssl_set($cn, NULL, NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-   mysqli_real_connect($cn, $host, $dbuser, $dbpass, $dbname, $port);
+    mysqli_real_connect($cn, $host, $dbuser, $dbpass, $dbname, $port);
 } catch (Exception $e) {
     echo $e->getMessage();
     die();
 }
-$sql="insert into reciver(firstname,lastname,contcactno,email,bloodgroup) values('$fname','$lname',$cno,'$email','$bgroup')";
-$result=$cn->query($sql);
-$rowcount=$result->rowCount();
-if($rowcount==1)
-{
-   ?>
+
+$sql = "INSERT INTO reciver (firstname, lastname, contcactno, email, bloodgroup) VALUES ('$fname', '$lname', '$cno', '$email', '$bgroup')";
+$result = mysqli_query($cn, $sql);
+
+if ($result) {
+    ?>
     <script type="text/javascript">alert('Successfully Registered !!!');</script>
     <?php
     header("location:home.php");
-}
-else 
-{
+} else {
     ?>
-    <script type="text/javascript">alert('please register First !!!');</script>
+    <script type="text/javascript">alert('Failed to register. Please try again.');</script>
     <?php
     header("location:register1.html");
 }
+
+mysqli_close($cn);
 ?>
