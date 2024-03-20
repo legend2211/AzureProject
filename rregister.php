b6b6d095158390e81dfda47c3ea392524fffbbf4
Dnyanesh Agarkar <121270109+legend2211@users.noreply.github.com>
2024-03-20 19:54:47 +0530

    Update rregister.php

--- rregister.php
+++ rregister.php
@@ -4,23 +4,21 @@
   $cno=$_POST['cno'];
   $email=$_POST['email'];
   $bgroup=$_POST['bgroup'];
-  try
-{
-    /*$host="localhost";
-    $port=3306;
-    $dbname="project";
-    $dbuser="root";
-    $dbpass="root";
+ try {
+    $host = "myazsqldemo.mysql.database.azure.com";
+    $port = 3306;
+    $dbname = "project";
+    $dbuser = "myadmin@myazsqldemo";
+    $dbpass = "Server@1";
 
-    $cn=new PDO("mysql:host=$host; port=$port; dbname=$dbname",$dbuser,$dbpass);*/
-   $cn=mysqli_init(); mysqli_ssl_set($cnn, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "myazsqldemo.mysql.database.azure.com", "myadmin@myazsqldemo", {Server@1}, {project}, 3306);
-}
-
-catch(PDOException $e)
-{
-    echo $e-> getmessage();
+    $cn = mysqli_init();
+    mysqli_ssl_set($cn, NULL, NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+    mysqli_real_connect($cn, $host, $dbuser, $dbpass, $dbname, $port);
+} catch (Exception $e) {
+    echo $e->getMessage();
     die();
 }
+
 
 $sql="insert into reciver(firstname,lastname,contcactno,email,bloodgroup) values('$fname','$lname',$cno,'$email','$bgroup')";
 $result=$cn->query($sql);
