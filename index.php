d58b673891abda6921b534c17c347ab14dcfbb3e
Dnyanesh Agarkar <121270109+legend2211@users.noreply.github.com>
2024-03-27 15:39:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,130 +1,100 @@
-<!Doctype html>
+<!DOCTYPE html>
 <html>
 
 <head>
+    <title>Blood Bank System</title>
     <style>
-        footer {
-  background-color: #F1F3F5;
-  padding: 20px 0;
-}
+        body {
+            background-color: whitesmoke;
+            margin: 0;
+            padding: 0;
+        }
 
-.container {
-  max-width: 960px;
-  margin: 0 auto;
-}
+        .topnav {
+            background-color: #333;
+            overflow: hidden;
+        }
 
-.footer-content {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
+        .topnav a {
+            float: left;
+            color: white;
+            text-align: center;
+            padding: 14px 20px;
+            text-decoration: none;
+            font-size: 17px;
+        }
 
-.contact, .social-media {
-  width: 45%;
-  text-align: center;
-}
+        .topnav a:hover {
+            background-color: #ddd;
+            color: black;
+        }
 
-.contact ul, .social-media ul {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
+        .active {
+            background-color: #4CAF50;
+        }
 
-.contact ul li, .social-media ul li {
-  margin-bottom: 10px;
-}
+        h1 {
+            text-align: center;
+        }
 
-.social-media a {
-  color: #000;
-  text-decoration: none;
-  font-weight: bold;
-}
-        </style>
-        </head>
-    <center>
+        p {
+            text-align: justify;
+            padding: 0 20px;
+        }
 
-<body bgcolor="whitesmoke">
-    <form name="Home" method="post">
-        <table>
-            <title>Blood Bank System</title>
-            <link rel="stylesheet" type="text/css" href="style2.css" />
-            <div class="topnav">
-                <a class="active " href="index.php">Home</a>
-                <a class="active " href="register1.html">Register</a> 
-                <a class="active " href="certificate.html">Certificate</a>
-                <a class="active" href="rregister.html">Request</a>     
-                <a class="active" href="contactus.php">contact us</a>
-             
-            </div>
+        ol {
+            padding-left: 20px;
+        }
 
-            <body>
-            <img src="pic2.jpg"  width:="400px" height="200px">
-                <tr>
-                    <td> 
-                        <h1><u>Blood Group</u>
-                            <h1>
-                    </td>
-                </tr>
-                <tr><br></tr>
-<iframe src='https://webchat.botframework.com/embed/myazurelangservice-bot?s=QTo3OW4B30Q.vXQpz9bd3j7p1OAQ06NvjoRRPaAmuPkRD1XT7AJQvtQ'  style='min-width: 400px; width: 100%; min-height: 500px;'></iframe>
+        img {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
 
-                <tr>
-                    <td>
-                        
-                        <p>The human body contains around 8 to 10 pints of blood depending on the size of the
-                            individual. However, the composition of the blood is not the same in each person. This is
-                            what makes the person's blood type.</p><br>
-                        <p>There are four ABO groups:</p>
-                        <ol>
-                            <li>Group A: The surface of the red blood cells contains A antigen, and the plasma has
-                                anti-B antibody that would attack any foreign B antigen containing red blood cells.</li>
+        iframe {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            margin-bottom: 20px;
+            border: none;
+            width: 80%;
+            height: 500px;
+        }
+    </style>
+</head>
 
-                            <li>Group B: The surface of the red blood cells contains B antigen, and the plasma has
-                                anti-A antibody that would attack any foreign A antigen containing red blood cells.</li>
+<body>
+    <div class="topnav">
+        <a class="active" href="index.php">Home</a>
+        <a class="active" href="register1.html">Register</a>
+        <a class="active" href="certificate.html">Certificate</a>
+        <a class="active" href="rregister.html">Request</a>
+        <a class="active" href="contactus.php">Contact Us</a>
+    </div>
 
-                            <li>Group AB: The red blood cells have both A and B antigens, but the plasma does not
-                                contain anti-A/anti-B antibodies. Individuals with type AB can receive any ABO blood
-                                type.</li>
+    <h1>Blood Group</h1>
 
-                            <li>Group O: The plasma contains both types of anti-A/anti-B antibodies, but the surface of
-                                the red blood cells does not contain any A/B antigens. Having none of these A/B antigens
-                                means that they can be donated to a person with any ABO blood type.</li>
-                        </ol>
-                    </td>
-                </tr>
-                <tr><br></tr>
-</table>
-                <img src="pic3.jpg"  width:="400px" height="200px">
-                <table>
-                <tr>
-                    <td>
-                        <h1><u>Why Donate Blood?</u>
-                            <h1>
-                    </td>
-                </tr>
-                <tr><br></tr>
+    <img src="pic2.jpg" alt="Blood Donation" width="400px" height="200px">
 
-                <tr>
-                    <td>
-                        <p>A blood donation truly is a “gift of life” that a healthy individual can give to others in
-                            their community who are sick or injured. In one hour’s time, a person can donate one unit of
-                            blood that can be separated into four individual components that could help save multiple
-                            lives.</p><br>
+    <iframe src="https://webchat.botframework.com/embed/myazurelangservice-bot?s=QTo3OW4B30Q.vXQpz9bd3j7p1OAQ06NvjoRRPaAmuPkRD1XT7AJQvtQ"></iframe>
 
-                        <p>From one unit of blood, red blood cells can be extracted for use in trauma or surgical
-                            patients. Plasma, the liquid part of blood, is administered to patients with clotting
-                            problems. The third component of blood, platelets, clot the blood when cuts or other open
-                            wounds occur, and are often used in cancer and transplant patients. Cryoprecipitated
-                            anti-hemophilic factor (AHF) is also used for clotting factors.</p>
-                    </td>
-                </tr>
-                <tr><br></tr>
-</table>
-                
-           </body>
-       
-    </form>
-    </center>
-    
+    <p>The human body contains around 8 to 10 pints of blood depending on the size of the individual. However, the composition of the blood is not the same in each person. This is what determines the person's blood type.</p>
+    <p>There are four ABO groups:</p>
+    <ol>
+        <li>Group A: The surface of the red blood cells contains A antigen, and the plasma has anti-B antibody that would attack any foreign B antigen containing red blood cells.</li>
+        <li>Group B: The surface of the red blood cells contains B antigen, and the plasma has anti-A antibody that would attack any foreign A antigen containing red blood cells.</li>
+        <li>Group AB: The red blood cells have both A and B antigens, but the plasma does not contain anti-A/anti-B antibodies. Individuals with type AB can receive any ABO blood type.</li>
+        <li>Group O: The plasma contains both types of anti-A/anti-B antibodies, but the surface of the red blood cells does not contain any A/B antigens. Having none of these A/B antigens means that they can be donated to a person with any ABO blood type.</li>
+    </ol>
+
+    <img src="pic3.jpg" alt="Blood Donation" width="400px" height="200px">
+
+    <h1>Why Donate Blood?</h1>
+    <p>A blood donation truly is a “gift of life” that a healthy individual can give to others in their community who are sick or injured. In one hour’s time, a person can donate one unit of blood that can be separated into four individual components that could help save multiple lives.</p>
+    <p>From one unit of blood, red blood cells can be extracted for use in trauma or surgical patients. Plasma, the liquid part of blood, is administered to patients with clotting problems. The third component of blood, platelets, clot the blood when cuts or other open wounds occur, and are often used in cancer and transplant patients. Cryoprecipitated anti-hemophilic factor (AHF) is also used for clotting factors.</p>
+</body>
 
 </html>
